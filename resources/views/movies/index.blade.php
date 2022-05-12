9ba788aac248f65c979b554774d05165f7785810
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-12 16:34:09 +0200

    cambiata routes che porta a visualizzare l'index.blade

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.main')
 
+@section("title", "Laravel Model Controller ")
+
 @section("main-content")
     <div class="container-fluid">
-
+        <section class="row">
+            <div class="col-12">
+                <p>ciao</p>
+            </div>
+        </section>
 
     </div>
 
