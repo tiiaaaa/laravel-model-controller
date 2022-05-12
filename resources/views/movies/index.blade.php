9764d1a4850746f88e206ada9e926b77b370bc3b
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-12 18:43:33 +0200

    stilizzate un minimo le singole card

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,11 +4,22 @@
 
 @section("main-content")
     <div class="container-fluid">
-        <section class="row">
+        <section class="row p-5">
+            {{-- foreach che cicla su movies e restituisce ogni singolo film --}}
             @foreach ($movies as $movie)
                 <div class="col-3">
-                    <h2>{{ $movie->title }}</h2>
-                    <p>{{ $movie->original_title }}</p>
+                    {{-- Singola Card --}}
+                    <div class="card mb-4">
+                        {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                        <div class="card-body text-center">
+                            <h3 class="card-title fw-bold">{{ $movie->title }}</h3>
+                            <p class="card-text fs-5 m-0">{{ $movie->original_title }}</p>
+                            <p class="mb-1">{{ $movie->nationality }}</p>
+                            <pre class="fs-6 m-0">{{ $movie->date }}</pre>
+                            <pre class="fs-6 m-0">{{ $movie->vote }}</pre>
+                            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                        </div>
+                    </div>
                 </div>
             @endforeach
         </section>
